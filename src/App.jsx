601b254d7d1ba860b8993d2786dd3079c37453f1
Oreidasovas <gabrielsuporteti2@gmail.com>
2025-07-29 11:52:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <div style={{ display: "flex" }}>
-        <Sidebar />
-        <main style={{ flex: 1, padding: "1rem" }}>
-          <h2>Bem-vindo ao sistema de controle de estoque!</h2>
-        </main>
-      </div>
-    </div>
+    <Layout>
+      <Dashboard />
+    </Layout>
   );
 }
