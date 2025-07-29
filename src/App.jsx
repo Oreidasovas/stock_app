2b5505ffcdf822ab53b646c00235559c72cc6637
Oreidasovas <gabrielsuporteti2@gmail.com>
2025-07-29 11:51:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 export default function App() {
-  return <h1>Controle de Estoque - O Rei das Ovas</h1>;
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <main style={{ flex: 1, padding: "1rem" }}>
+          <h2>Bem-vindo ao sistema de controle de estoque!</h2>
+        </main>
+      </div>
+    </div>
+  );
 }
